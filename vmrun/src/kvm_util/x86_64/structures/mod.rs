ade0dffd1e42df39b49d8281054e848c6dd8c394
Harald Hoyer <harald@redhat.com>
2019-11-27 17:41:04 +0100

    remove even more unused code

--- vmrun/src/kvm_util/x86_64/structures/mod.rs
+++ vmrun/src/kvm_util/x86_64/structures/mod.rs
@@ -1,14 +1,3 @@
 //! Representations of various x86 specific structures and descriptor tables.
 
 pub mod paging;
-
-/// A struct describing a pointer to a descriptor table (GDT / IDT).
-/// This is in a format suitable for giving to 'lgdt' or 'lidt'.
-#[derive(Debug, Clone, Copy)]
-#[repr(C, packed)]
-pub struct DescriptorTablePointer {
-    /// Size of the DT.
-    pub limit: u16,
-    /// Pointer to the memory region containing the DT.
-    pub base: u64,
-}
--- vmrun/src/kvm_util/x86_64/structures/paging/mod.rs
+++ vmrun/src/kvm_util/x86_64/structures/paging/mod.rs
@@ -3,18 +3,9 @@
 //! Page tables translate virtual memory “pages” to physical memory “frames”.
 
 pub use self::frame::PhysFrame;
-pub use self::frame_alloc::{FrameAllocator, FrameDeallocator};
-#[doc(no_inline)]
-pub use self::mapper::MappedPageTable;
-pub use self::mapper::{Mapper, MapperAllSizes};
-#[cfg(target_arch = "x86_64")]
-#[doc(no_inline)]
-pub use self::mapper::{OffsetPageTable, RecursivePageTable};
 pub use self::page::{Page, PageSize, Size1GiB, Size2MiB, Size4KiB};
 pub use self::page_table::{PageTable, PageTableFlags};
 
 pub mod frame;
-mod frame_alloc;
-pub mod mapper;
 pub mod page;
 pub mod page_table;
