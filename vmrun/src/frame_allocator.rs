2f18c1b3bb7de028053d85a491e264b921af14a4
Harald Hoyer <harald@redhat.com>
2020-01-30 16:07:03 +0100

    vmrun/src/frame_allocator.rs: fixed merging of segments

--- vmrun/src/frame_allocator.rs
+++ vmrun/src/frame_allocator.rs
@@ -9,19 +9,23 @@ impl FrameAllocator {
     ///
     /// Panics if a non-usable region (e.g. a reserved region) overlaps with the passed region.
     pub(crate) fn mark_allocated_region(&mut self, region: MemoryRegion) {
+        let mut region = region.clone();
         for r in self.memory_map.iter_mut() {
+            // New region inside region of same type
             if r.region_type == region.region_type
-                && r.range.start_frame_number == region.range.start_frame_number
-                && r.range.end_frame_number == region.range.end_frame_number
+                && r.range.start_frame_number <= region.range.start_frame_number
+                && r.range.end_frame_number >= region.range.end_frame_number
             {
                 return;
             }
 
+            // New region extends old region
             if r.region_type == region.region_type
-                && r.range.end_frame_number >= region.range.start_frame_number
+                && r.range.start_frame_number <= region.range.start_frame_number
+                && r.range.end_frame_number > region.range.start_frame_number
                 && r.range.end_frame_number <= region.range.end_frame_number
             {
-                r.range.end_frame_number = region.range.start_frame_number;
+                region.range.start_frame_number = r.range.end_frame_number;
             }
 
             if region.range.start_frame_number >= r.range.end_frame_number {
