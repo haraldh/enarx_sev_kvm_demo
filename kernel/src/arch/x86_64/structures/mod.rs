8757dd2bc0e24fc2e6a555c0a09b8f2fa5a61de9
Harald Hoyer <harald@redhat.com>
2020-03-19 17:25:44 +0100

    kernel/src/arch/x86_64/structures/mod.rs: fixup doc comments

--- kernel/src/arch/x86_64/structures/mod.rs
+++ kernel/src/arch/x86_64/structures/mod.rs
@@ -1,8 +1,7 @@
-//! Duplicated from the x86_64 crate, because of some modifications
+//! Duplicated from the x86_64 crate, because we want USER_ACCESSIBLE in newly allocated page tables
 //!
 //! can be removed, if we can get patches upstream
 //! see https://github.com/rust-osdev/x86_64/pull/114
 
-// Duplicated, because we want USER_ACCESSIBLE in newly allocated page tables
 pub mod paging;
 pub use paging::OffsetPageTable;
