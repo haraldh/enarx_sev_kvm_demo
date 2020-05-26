1a36ba1ca5244e9a377cd779c8e64a22cb238722
Harald Hoyer <harald@redhat.com>
2020-05-26 22:14:17 +0200

    ii

--- .github/scripts/run-remote-ci.sh
+++ .github/scripts/run-remote-ci.sh
@@ -1,5 +1,8 @@
 #/bin/bash
-cat > id_github_test <<EOF
+mkdir -m 0700 .ssh
+cat > .ssh/id_github_test <<EOF
 $SUPER_SECRET
 EOF
-ssh -T -i id_github_test fedorabook@hoyer.xyz
+chmod 0600 .ssh/id_github_test
+set -x
+ssh -vvv -T -i .ssh/id_github_test fedorabook@hoyer.xyz
