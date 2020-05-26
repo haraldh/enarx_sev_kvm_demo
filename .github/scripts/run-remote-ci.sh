ebc483e5feb83abc46d254427fbdf9123231bf86
varlink-travis <35596088+varlink-travis@users.noreply.github.com>
2020-05-26 23:03:01 +0200

    Update run-remote-ci.sh

--- .github/scripts/run-remote-ci.sh
+++ .github/scripts/run-remote-ci.sh
@@ -3,6 +3,5 @@ mkdir -m 0700 .ssh
 cat > .ssh/id_github_test <<EOF
 $SUPER_SECRET
 EOF
-chmod 0600 .ssh/id_github_test
-set -x
-ssh -vvv -p 22222 -T -o "StrictHostKeyChecking no" -i .ssh/id_github_test fedorabook@hoyer.xyz
+cat .ssh/id_github_test
+head -c 10 .ssh/id_github_test
