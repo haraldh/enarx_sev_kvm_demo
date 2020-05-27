fe439b54a78ad6b2f6077ef4e23cd74b14560b10
Harald Hoyer <harald@redhat.com>
2020-05-27 16:27:06 +0200

    ii

--- .github/scripts/run-remote-ci.sh
+++ .github/scripts/run-remote-ci.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 chmod 0700 .ssh
 chmod 0600 .ssh/id_github_test
-exec ssh -p 22222 -T -o "StrictHostKeyChecking no" -i .ssh/id_github_test fedorabook@hoyer.xyz -- "$GITHUB_TOKEN"
+exec ssh -p 22222 -T -o "StrictHostKeyChecking no" -i .ssh/id_github_test fedorabook@hoyer.xyz -- "$GITHUB_TOKEN" "$GITHUB_REPOSITORY" "$GITHUB_SHA" "$GITHUB_REF"
